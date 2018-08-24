25174a7c66fa4f1863bec2c2f7724bc3fba36f68
srich.leeds@gmail.com <srich.leeds@gmail.com>
2018-08-24 08:21:08 +0100

    New allocate screens including Unallocated and Search

--- app/routes.js
+++ app/routes.js
@@ -9,4 +9,6 @@ router.get('/', function (req, res) {
 // Add your routes here - above the module.exports line
 require('./views/v1/routes')(router, 'v1')
 
+require('./views/v2/routes')(router, 'v2')
+
 module.exports = router
